c2e3fa35fd0b79120d39d5b4a539f97a693caa79
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-09-10 12:31:06 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,10 +1,10 @@
 // Add your custom JavaScript for storefront pages here.
-if(window.innerWidth > 992) {
-  window.addEventListener('scroll',(event) => {
-    if(document.querySelector('.header').offsetTop > 60) {
-      document.querySelector('.header').classList.add('reduce-header')
-    } else {
-      document.querySelector('.header').classList.remove('reduce-header')
-    }
-  })
-}
+//if(window.innerWidth > 992) {
+//  window.addEventListener('scroll',(event) => {
+//    if(document.querySelector('.header').offsetTop > 60) {
+//      document.querySelector('.header').classList.add('reduce-header')
+//    } else {
+//      document.querySelector('.header').classList.remove('reduce-header')
+//    }
+//  })
+// }
