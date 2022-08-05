11499ac0ae41cedc18be0af8e632164066e3c0f2
Matheus Reis <matheusgnreis@gmail.com>
2022-08-04 21:45:11 -0300

    chore: modify layout

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,10 +1,14 @@
 // Add your custom JavaScript for storefront pages here.
-if(window.innerWidth > 992) {
+/* if(window.innerWidth > 992) {
   window.addEventListener('scroll',(event) => {
     if(document.querySelector('.header').offsetTop > 60) {
-      document.querySelector('.header').classList.add('reduce-header')
+      document.querySelector('.header').style.background = '#fff';
+      document.querySelector('.header').style.paddingTop = '0';
     } else {
-      document.querySelector('.header').classList.remove('reduce-header')
+      console.log('estou aqui')
+      console.log(document.querySelector('.header').offsetTop)
+      document.querySelector('.header').style.background = 'none !important'
+      document.querySelector('.header').style.paddingTop = '20px';
     }
   })
-}
+} */
